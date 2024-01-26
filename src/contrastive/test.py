8c8eee85cc90f539b51e825a62faa2544423fdd1
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-01-26 01:21:31 -0500

    update the test script

--- src/contrastive/test.py
+++ src/contrastive/test.py
@@ -4,7 +4,7 @@ import torch
 from model.model import ContrastiveModel
 import os
 import lightning.pytorch as pl
-from dataloader.sen_sim_simple import ContrastiveDataModule
+from dataloader.sen_sim_pair import ContrastiveDataModule
 from dataloader.doc_sim import ContrastiveDocDataModule
 
 
@@ -13,6 +13,10 @@ ROOT_DIR = "/nfs/ada/ferraro/users/sroydip1/semeval24/task8/subtaskB"
 if len(sys.argv) < 2:
     print("Please provide experiment name!")
     sys.exit()
+
+final = ""
+if len(sys.argv) > 2:
+    final = "_final"
 
 exp_name = sys.argv[1]
 files = os.listdir(f"{ROOT_DIR}/{exp_name}")
@@ -48,7 +52,7 @@ elif config.encoder_type == "sen":
 else:
     raise ValueError(f"Invalid encoder type: {config.encoder_type}")
 
-datamodule.setup("test")
+datamodule.setup(f"test{final}")
 
 trainer = pl.Trainer()
 
@@ -63,14 +67,17 @@ predicitons = [torch.tensor(pred) for pred in predicitons]
 ids = torch.cat(ids)
 predicitons = torch.cat(predicitons)
 
-
 print(ids.shape)
 print(predicitons.shape)
 
-with jsonlines.open(f"./out/out_{exp_name}.jsonl", "w") as writer:
+out_dir = "./out"
+if final:
+    out_dir = "./final"
+
+with jsonlines.open(f"{out_dir}/out_{exp_name}.jsonl", "w") as writer:
     for id, pred in zip(ids, predicitons):
         writer.write({"id": id.item(), "label": pred.item()})
 
-
-cmd = f"PYTHONPATH=../../ python ../../subtaskB/scorer/scorer.py --pred_file_path=./out/out_{exp_name}.jsonl --gold_file_path=./data/SubtaskB/subtaskB_dev.jsonl"
-os.system(cmd)
+if not final:
+    cmd = f"PYTHONPATH=../../ python ../../subtaskB/scorer/scorer.py --pred_file_path=./out/out_{exp_name}.jsonl --gold_file_path=./data/SubtaskB/subtaskB_dev.jsonl"
+    os.system(cmd)
